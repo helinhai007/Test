8d6d3a622236ec055e781384807b27287847407e
helinhai <719278821@qq.com>
2020-08-02 15:55:42 +0800

    http实现

--- fileServer.cpp
+++ fileServer.cpp
@@ -11,6 +11,7 @@
 #include <stdarg.h>
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
  
 #define BUF_SIZE 1024
@@ -21,13 +22,7 @@ int main(){
     //先检查文件是否存在
 
     //char *filename = "123.txt"; //文件名
-    FILE *fp = fopen("123.txt", "wb"); //以二进制方式打开文件
-
-    if(fp == NULL){
-    printf("Cannot open file, press any key to exit!\n");
-    exit(0);
-
-    }
+    
 
 
     int servSock = socket(AF_INET, SOCK_STREAM, 0);
@@ -61,44 +56,66 @@ int main(){
     struct sockaddr_in clientaddr;  //客户端的地址信息
 
     int clntSock = accept(servSock, (struct sockaddr *)&clientaddr, (socklen_t*)&socklen);
+    if(clntSock == -1)
+    {
+      cout << "accept failed" << endl;
+      return 0;
+    }
 
+    string str;
+    str = "HTTP/1.1 200 OK\r\n";
+    str = str + "Access-Control-Allow-Origin: null\r\n";
+    str = str + "Access-Control-Allow-Methods: POST, GET, OPTIONS\r\n";
+    str = str + "Access-Control-Allow-Headers: X-PINGOTHER, Content-Type\r\n";
+    str = str + "Content-Encoding: gzip, deflate\r\n";
+    str = str + "Connection: Keep-Alive\r\n";
+    cout << str << endl;
+
+    int nCount1 = send(clntSock, str.c_str(),str.size(), 0);
     cout << "accept sucess" << endl;
-    
-    //循环发送数据，直到文件结尾
 
-    // char buffer[BUF_SIZE] = {0}; //缓冲区
-
-    // int nCount;
-
-    // while( (nCount = fread(buffer, 1, BUF_SIZE, fp)) > 0 ){
-
-    // send(clntSock, buffer, nCount, 0);
-
-    // }
-    // cout << "send sucess" << endl;
-    
-    // shutdown(clntSock, 1); //文件读取完毕，断开输出流，向客户端发送FIN包
-
-    // recv(clntSock, buffer, BUF_SIZE, 0); //阻塞，等待客户端接收完毕
-
-    //循环接收数据，直到文件传输完毕
-
-  char buffer[BUF_SIZE] = {0}; //文件缓冲区
-
-  int nCount;
-
-  while( (nCount = recv(clntSock, buffer, BUF_SIZE, 0)) > 0 ){
-
-  fwrite(buffer, nCount, 1, fp);
-
-  }
-
-  cout << "recv sucess" << endl;
-
-    
-    fclose(fp);
+   if(nCount1 <=0)  
+   {
+     cout << "send: failed" << endl;
+     return 0;
+   }
+    cout << "send: sucess" << endl;
 
     close(clntSock);
+
+  while(1){
+
+    int clntSock = accept(servSock, (struct sockaddr *)&clientaddr, (socklen_t*)&socklen);
+    if(clntSock == -1)
+    {
+      continue;
+      cout << "clntSock == -1" << endl;
+    }
+
+    
+    //循环接收数据，直到文件传输完毕
+  char buffer[BUF_SIZE] = {0}; //文件缓冲区
+  int nCount = recv(clntSock, buffer, BUF_SIZE, 0);
+   cout<< nCount << endl;
+    string szTemp("");  
+    szTemp = "";  
+    szTemp += buffer;  
+    cout << szTemp;  
+
+  FILE *fp = fopen("123.txt", "wb"); //以二进制方式打开文件
+
+    if(fp == NULL){
+    printf("Cannot open file, press any key to exit!\n");
+    exit(0);
+    }
+  fwrite(buffer, nCount, 1, fp);
+  cout << "fwrite: sucess" << endl;
+  close(clntSock);
+  fclose(fp);
+  }
+
+
+  cout << "recv sucess" << endl;
 
     close(servSock);
 
